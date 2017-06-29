4a9629600f5b7f9154265b965b6dce7be485d5bd
Arjen Janssen <arjenjanssen@gmail.com>
2017-06-29 14:43:03 +0200

    just a comit

---
+++ _site/js/receptAnimations.js
@@ -0,0 +1,25 @@
+// ----------set nr of persons for recipe
+var nrOfPStart = 4
+// change number of people in h2
+$("input[type='range']").on("input change", function(event){
+  $(".personen").text($("input[type='range']").val());
+  });
+// change volumes in ul
+$("input[type='range']").on("change", function(event){
+  // grab value from range input
+  var nrOfP = $(this).val();
+  // multiply input with volumes
+  $("b").each(function(){
+    var newVolume = ($(this).text() / nrOfPStart * nrOfP);
+    $(this).text(Math.round(newVolume*10)/10);
+  });
+  // update totle nr of persones
+  $("#personen").text(nrOfP);
+  // update nrOfPStart
+  nrOfPStart = nrOfP;
+});
+
+// ----------check off specific ingredient by clicking
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
