442c56dfea5103b190eb87d11247ea10403ceb2f
Arjen Janssen <arjenjanssen@gmail.com>
2017-07-02 22:57:58 +0200

    reatie form Recept blog en weetje

--- _site/assets/tipuesearch/tipuesearch_content.js
+++ _site/assets/tipuesearch/tipuesearch_content.js
@@ -63,7 +63,7 @@ var tipuesearch = {"pages": [
   
   {
     "title": "Contact",
-    "text": "",
+    "text": "Mail ons, wij reageren graag persoonlijk! Wil je meer weten over ‘Bloei en Leef’-voedingsstijl, -beweegstijl en -leefstijl? Wij vertellen je graag meer over: Hormoonfactor Training onze Hart Verwen Weekenden en Retraites Massage of Yoga een Inspiratie Sessie of Coaching en advies gezonde Weetjes of Recepten; onze 12-delige Voedingscursus inclusief kooklessen Culinaire Fotografie Onszelf Wij kiezen bewust voor mail of persoonlijk contact en niet voor een bestelformulier of webshop. Op deze manier blijft het voor jou en voor ons persoonlijker. Mail of bel ons gerust, zodat we nader kennis kunnen maken onder het genot van een heerlijke kop van onze eigenverbouwde thee of een kop biologische koffie. Tip! Als je op de hoogte wilt blijven van ons laatste nieuws, onze weekenden en retraites, recepten, kooklessen en meer? Schrijf je dan vrijblijvend in voor onze nieuwsbrief door onderstaand contactformulier in te vullen. Verwen je Blij! Adres: Bloei en Leef, Citroenvlinder 106, 7943 RC Meppel Telefoon: 06-11040769 Kvk: 60865482",
     "tags": "",
     "url": "/page/contact/"
   },
