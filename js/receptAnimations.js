0f5b0afd4a9bfdd34cda8d63abdc4d5dbdc78280
Arjen Janssen <arjenjanssen@gmail.com>
2017-07-04 16:38:45 +0200

    swipe swipe

--- js/receptAnimations.js
+++ js/receptAnimations.js
@@ -30,9 +30,9 @@ function swipedetect(el, callback){
     var touchsurface = el,
     swipedir,
     startX,
-    startY,
+    // startY,
     distX,
-    distY,
+    // distY,
     threshold = 150, //required min distance traveled to be considered swipe
     restraint = 100, // maximum distance allowed at the same time in perpendicular direction
     allowedTime = 300, // maximum time allowed to travel that distance
@@ -45,30 +45,20 @@ function swipedetect(el, callback){
         swipedir = 'none'
         dist = 0
         startX = touchobj.pageX
-        startY = touchobj.pageY
         startTime = new Date().getTime() // record time when finger first makes contact with surface
-        e.preventDefault()
     }, false)
 
-    touchsurface.addEventListener('touchmove', function(e){
-        e.preventDefault() // prevent scrolling when inside DIV
-    }, false)
 
     touchsurface.addEventListener('touchend', function(e){
         var touchobj = e.changedTouches[0]
         distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
-        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
         elapsedTime = new Date().getTime() - startTime // get time elapsed
         if (elapsedTime <= allowedTime){ // first condition for awipe met
-            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+            if (Math.abs(distX) >= threshold ){ //2nd condition for horizontal swipe met
                 swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
             }
-            // else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-            //     swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
-            // }
         }
         handleswipe(swipedir)
-        e.preventDefault()
     }, false)
 }
 
@@ -76,7 +66,9 @@ function swipedetect(el, callback){
 
 var el = document.getElementById('receptSection')
 swipedetect(el, function(swipedir){
-    //  swipedir contains either "none", "left", "right", "top", or "down"
+    //  swipedir contains either "none", "left", "right",
     if (swipedir =='left')
-        alert('You just swiped left!')
+        alert('You just swiped left!');
+    if (swipedir =='right')
+        alert('You just swiped right!');
 })
