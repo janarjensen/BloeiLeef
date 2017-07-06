90a79c71ecef53f92af61855b8341c263eb62e7d
janarjensen <arjenjanssen@gmail.com>
2017-07-06 09:50:26 +0200

    swipe test 5

--- js/receptAnimations.js
+++ js/receptAnimations.js
@@ -71,9 +71,9 @@ var el = document.getElementById('receptSection')
 swipedetect(el, function(swipedir){
     //  swipedir contains either "none", "left", "right",
     if (swipedir =='left')
-        location.href = (origin + $('.previous').attr('href'));
+        location.href = (origin + "/BloeiLeef" + $('.previous').attr('href'));
     if (swipedir =='right')
-        location.href = (origin + $('.next').attr('href'));
+        location.href = (origin + "/BloeiLeef" + $('.next').attr('href'));
 })
 
 // console.log(origin + $('.previous').attr('href'));
